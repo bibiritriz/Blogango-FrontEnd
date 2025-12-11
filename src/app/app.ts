5e14eb88c848d726da518a23f510bd909565f7c2
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-12-10 22:34:45 -0300

    adiciona como variaveis o titulo, conteudo, autor, etc no component post

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header';
+import { PostComponent } from './components/shared/post/post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, PostComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/components/shared/post/post.ts
+++ src/app/components/shared/post/post.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
-export class PostComponent {}
+export class PostComponent {
+  titulo = 'Tutorial de Spring Boot com MongoDB';
+
+  data = '04/12/2025';
+
+  autor = 'Autor';
+
+  conteudo = 'Este é um conteúdo de teste criado para validar a restrição de no míni…';
+
+  categorias = ['Java', 'Spring Boot', 'MongoDB'];
+}
