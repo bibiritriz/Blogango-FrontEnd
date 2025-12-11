19cd26cb77fe4b4c4b9929c43ca7ca2cee9aeb6e
Isaque Barbosa <isaquebatista400@gmail.com>
2025-12-10 21:01:57 -0300

    :sparkles: feat: Adicionando configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [{}];
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Teste } from './components/teste/teste';
+// TODO: Adicinoar comopnentes respectivos
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', redirectTo: 'posts', pathMatch: 'full' },
+      {
+        path: 'posts',
+        title: 'Blogango - Posts',
+        component: Teste,
+      },
+      /*
+      {
+        path: 'posts/novo',
+        title: 'Criar Novo Post',
+      },
+      {
+        path: 'posts/editar/:id',
+        title: 'Editar Post',
+      },
+      {
+        path: 'posts/:slug',
+        title: 'Lendo Artigo',
+      },
+      {
+        path: 'rascunhos',
+        title: 'Meus Rascunhos',
+      },
+      {
+        path: 'rascunhos/editar/:id',
+        title: 'Editar Rascunho',
+      },
+      {
+        path: 'comentarios',
+        title: 'Gerenciar Comentários',
+      },
+      */
+    ],
+  },
+];
