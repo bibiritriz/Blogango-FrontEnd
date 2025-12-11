621655ad3ecfa37505b36832dc2a1aaecd5eb6d3
Isaque Barbosa <isaquebatista400@gmail.com>
2025-12-10 22:51:30 -0300

    :bug: fix: Arrumando serviço de obter comentarios por post e Tipagem de criarComentario

--- src/app/services/comentario/comentario.service.ts
+++ src/app/services/comentario/comentario.service.ts
@@ -1,7 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Comentario, ComentarioUpdateDTO } from '../../models/comentario.model';
+import {
+  Comentario,
+  ComentarioCreateDTO,
+  ComentarioUpdateDTO,
+} from '../../models/comentario.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +15,11 @@ export class ComentarioService {
 
   private readonly url = 'http://localhost:8080/api/comentarios';
 
-  public obterComentario(comentarioId: string): Observable<Comentario[]> {
-    return this.http.get<Comentario[]>(`${this.url}/${comentarioId}`);
+  public obterComentario(postId: string): Observable<Comentario[]> {
+    return this.http.get<Comentario[]>(`${this.url}/${postId}`);
   }
 
-  public criarComentario(comentario: Comentario): Observable<Comentario> {
+  public criarComentario(comentario: ComentarioCreateDTO): Observable<Comentario> {
     return this.http.post<Comentario>(this.url, comentario);
   }
 
